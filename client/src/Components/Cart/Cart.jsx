fd1ad8e7ad107b4cb324c411e1e1623c31b6efc7
KartikNP31 <patelkn1303@gmail.com>
2023-08-04 19:35:47 +0530

    updates

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -1,54 +1,54 @@
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 
-// import { Box, Typography, Button, Grid, styled } from '@mui/material';
-// import { useParams } from 'react-router-dom';
+import { Box, Typography, Button, Grid, styled } from '@mui/material';
+import { useParams } from 'react-router-dom';
 
 import { useSelector, useDispatch } from 'react-redux';
 // import { addToCart, removeFromCart } from '../../redux/actions/cartActions';
 
-// import TotalView from './TotalView';
+import TotalView from './TotalView';
 // import EmptyCart from './EmptyCart';
-// import CartItem from './CartItem';
+import CartItem from './CartItem';
 
 // import { post } from '../../utils/paytm';
-// import { payUsingPaytm } from '../../service/api';
+import { payUsingPaytm } from '../../service/api';
 
-// const Component = styled(Grid)(({ theme }) => ({
-//     padding: '30px 135px',
-//     display: 'flex',
-//     [theme.breakpoints.down('sm')]: {
-//         padding: '15px 0'
-//     }
-// }));
+const Component = styled(Grid)(({ theme }) => ({
+    padding: '30px 135px',
+    display: 'flex',
+    [theme.breakpoints.down('sm')]: {
+        padding: '15px 0'
+    }
+}));
 
-// const LeftComponent = styled(Grid)(({ theme }) => ({
-//     paddingRight: 15,
-//     [theme.breakpoints.down('sm')]: {
-//         marginBottom: 15
-//     }
-// }));
+const LeftComponent = styled(Grid)(({ theme }) => ({
+    paddingRight: 15,
+    [theme.breakpoints.down('sm')]: {
+        marginBottom: 15
+    }
+}));
 
-// const Header = styled(Box)`
-//     padding: 15px 24px;
-//     background: #fff;
-// `;
+const Header = styled(Box)`
+    padding: 15px 24px;
+    background: #fff;
+`;
 
-// const BottomWrapper = styled(Box)`
-//     padding: 16px 22px;
-//     background: #fff;
-//     box-shadow: 0 -2px 10px 0 rgb(0 0 0 / 10%);
-//     border-top: 1px solid #f0f0f0;
-// `;
+const BottomWrapper = styled(Box)`
+    padding: 16px 22px;
+    background: #fff;
+    box-shadow: 0 -2px 10px 0 rgb(0 0 0 / 10%);
+    border-top: 1px solid #f0f0f0;
+`;
 
-// const StyledButton = styled(Button)`
-//     display: flex;
-//     margin-left: auto;
-//     background: #fb641b;
-//     color: #fff;
-//     border-radius: 2px;
-//     width: 250px;
-//     height: 51px;
-// `;
+const StyledButton = styled(Button)`
+    display: flex;
+    margin-left: auto;
+    background: #fb641b;
+    color: #fff;
+    border-radius: 2px;
+    width: 250px;
+    height: 51px;
+`;
 
 const Cart = () => {
     const cartDetails = useSelector(state => state.cart);
@@ -75,7 +75,7 @@ const Cart = () => {
     //     post(information);
     // }
 
-    return (
+    // return (
         // <>
         // { cartItems.length ? 
         //     <Component container>
@@ -98,7 +98,7 @@ const Cart = () => {
         // }
         // </>
 
-    )
+    // )
 }
 
 export default Cart;
